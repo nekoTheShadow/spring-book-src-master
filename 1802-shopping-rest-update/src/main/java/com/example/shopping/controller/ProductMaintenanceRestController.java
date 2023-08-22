2f243325d2624ac00c983d5004f155caf9f59e28
nekoTheShadow <h.nakamura0903@gmail.com>
2023-08-22 23:00:00 +0900

    第13章 : 更新系のREST APIの作成

--- 1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
+++ 1802-shopping-rest-update/src/main/java/com/example/shopping/controller/ProductMaintenanceRestController.java
@@ -1,13 +1,23 @@
 package com.example.shopping.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.shopping.entity.Product;
+import com.example.shopping.input.ProductMaintenanceInput;
 import com.example.shopping.service.ProductMaintenanceService;
 
 @RestController
@@ -25,7 +35,34 @@ public class ProductMaintenanceRestController {
     }
 
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable String id) {
-        return productMaintenanceService.findById(id);
+    public ResponseEntity<Product> getProduct(@PathVariable String id) {
+    	Product product = productMaintenanceService.findById(id);
+    	if (product==null) {
+    		return ResponseEntity.notFound().build();
+    	} else {
+    		return ResponseEntity.ok(product);
+    	}
+        
+    }
+    
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProduct(@PathVariable String id, @RequestBody ProductMaintenanceInput productMaintenanceInput) {
+    	productMaintenanceInput.setId(id);
+    	productMaintenanceService.update(productMaintenanceInput);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Void> registerProduct(@RequestBody ProductMaintenanceInput productMaintenanceInput) {
+    	Product product = productMaintenanceService.register(productMaintenanceInput);
+    	URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{id}").buildAndExpand(product.getId()).toUri();
+    	return ResponseEntity.created(location).build();
+    	
+    }
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id) {
+    	productMaintenanceService.delete(id);
     }
 }
