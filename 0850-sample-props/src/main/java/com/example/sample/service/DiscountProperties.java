ac5e6038a7b29e406c6ff9d35e44c53bc681f61b
nekoTheShadow <h.nakamura0903@gmail.com>
2023-08-27 23:00:00 +0900

    第23章 : プロパティの外部化

--- 0850-sample-props/src/main/java/com/example/sample/service/DiscountProperties.java
+++ 0850-sample-props/src/main/java/com/example/sample/service/DiscountProperties.java
@@ -1,5 +1,10 @@
 package com.example.sample.service;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "discount")
 public class DiscountProperties {
     private double rate;
     private int max;
--- 0850-sample-props/src/main/java/com/example/sample/service/DiscountServiceImpl.java
+++ 0850-sample-props/src/main/java/com/example/sample/service/DiscountServiceImpl.java
@@ -1,30 +1,33 @@
 package com.example.sample.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscountServiceImpl implements DiscountService {
 
-    /** ディスカウント率 */
-    @Value("${discount.rate}")
-    private double discountRate;
-
-    /** ディスカウント上限 */
-    @Value("${discount.max}")
-    private int discountMax;
+//    /** ディスカウント率 */
+//    @Value("${discount.rate}")
+//    private double discountRate;
+//
+//    /** ディスカウント上限 */
+//    @Value("${discount.max}")
+//    private int discountMax;
+	
+	@Autowired
+	private DiscountProperties discountProperties;
 
     @Override
     public int calculateDiscountPrice(int originalPrice) {
 
-        System.out.println("ディスカウント率:" + discountRate);
-        System.out.println("ディスカウント上限:" + discountMax);
+        System.out.println("ディスカウント率:" + discountProperties.getRate());
+        System.out.println("ディスカウント上限:" + discountProperties.getMax());
 
-        int discount = (int)(originalPrice * discountRate);
+        int discount = (int)(originalPrice * discountProperties.getRate());
 
-        if (discount > discountMax) {
-            System.out.println("ディスカウント額が上限値を超えました。discount=" + discount + " max=" + discountMax);
-            discount = discountMax;
+        if (discount > discountProperties.getMax()) {
+            System.out.println("ディスカウント額が上限値を超えました。discount=" + discount + " max=" + discountProperties.getMax());
+            discount = discountProperties.getMax();
         }
 
         return originalPrice - discount;
