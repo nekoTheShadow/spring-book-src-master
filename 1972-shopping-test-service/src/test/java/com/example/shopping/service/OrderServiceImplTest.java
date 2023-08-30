614ef2420b9cec303527370bd4be812a9660f987
nekoTheShadow <h.nakamura0903@gmail.com>
2023-08-30 23:00:00 +0900

    第26章 : Service のユニットテスト

--- 1972-shopping-test-service/src/test/java/com/example/shopping/service/OrderServiceImplTest.java
+++ 1972-shopping-test-service/src/test/java/com/example/shopping/service/OrderServiceImplTest.java
@@ -8,19 +8,21 @@ import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.shopping.input.CartItemInput;
-import com.example.shopping.input.OrderInput;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.shopping.entity.Order;
+import com.example.shopping.entity.OrderItem;
 import com.example.shopping.entity.Product;
 import com.example.shopping.enumeration.PaymentMethod;
 import com.example.shopping.exception.StockShortageException;
 import com.example.shopping.input.CartInput;
+import com.example.shopping.input.CartItemInput;
+import com.example.shopping.input.OrderInput;
 import com.example.shopping.repository.OrderItemRepository;
 import com.example.shopping.repository.OrderRepository;
 import com.example.shopping.repository.ProductRepository;
@@ -40,10 +42,18 @@ class OrderServiceImplTest {
     
     @Test
     void test_placeOrder() {
+    	ArgumentCaptor<Order> orderCaptor = ArgumentCaptor.forClass(Order.class);
+    	ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
+    	ArgumentCaptor<OrderItem> orderItemCaptor = ArgumentCaptor.forClass(OrderItem.class);
+    	doNothing().when(orderRepository).insert(orderCaptor.capture());
+    	doReturn(true).when(productRepository).update(productCaptor.capture());
+    	doNothing().when(orderItemRepository).insert(orderItemCaptor.capture());
         
         Product product1 = new Product();
+        product1.setId("p01");
         product1.setStock(10);
         Product product2 = new Product();
+        product2.setId("p02");
         product2.setStock(5);
         doReturn(product1).when(productRepository).selectById("p01");
         doReturn(product2).when(productRepository).selectById("p02");
@@ -85,7 +95,32 @@ class OrderServiceImplTest {
 
         verify(orderRepository).insert(any());
         verify(productRepository, times(2)).update(any());
-        verify(orderItemRepository, times(2)).insert(any());        
+        verify(orderItemRepository, times(2)).insert(any());
+        
+        // updateされた値を検査する
+        assertThat(productCaptor.getAllValues().get(0).getStock()).isEqualTo(7);
+        assertThat(productCaptor.getAllValues().get(1).getStock()).isEqualTo(1);
+        
+        // insertされた値を検査する。
+        assertThat(orderCaptor.getValue().getId()).isNotNull();
+        assertThat(orderCaptor.getValue().getOrderDateTime()).isNotNull();
+        assertThat(orderCaptor.getValue().getBillingAmount()).isEqualTo(1210);
+        assertThat(orderCaptor.getValue().getCustomerName()).isEqualTo("東京太郎");
+        assertThat(orderCaptor.getValue().getCustomerAddress()).isEqualTo("東京都");
+        assertThat(orderCaptor.getValue().getCustomerPhone()).isEqualTo("090-0000-0000");
+        assertThat(orderCaptor.getValue().getCustomerEmailAddress()).isEqualTo("taro@example.com");
+        assertThat(orderCaptor.getValue().getPaymentMethod()).isEqualTo(PaymentMethod.CONVENIENCE_STORE);
+        
+        // insertされた値を検査する。
+        assertThat(orderItemCaptor.getAllValues().get(0).getOrderId()).isEqualTo(orderCaptor.getAllValues().get(0).getId());
+        assertThat(orderItemCaptor.getAllValues().get(0).getProductId()).isEqualTo("p01");
+        assertThat(orderItemCaptor.getAllValues().get(0).getPriceAtOrder()).isEqualTo(100);
+        assertThat(orderItemCaptor.getAllValues().get(0).getQuantity()).isEqualTo(3);
+        
+        assertThat(orderItemCaptor.getAllValues().get(1).getOrderId()).isEqualTo(orderCaptor.getAllValues().get(0).getId());
+        assertThat(orderItemCaptor.getAllValues().get(1).getProductId()).isEqualTo("p02");
+        assertThat(orderItemCaptor.getAllValues().get(1).getPriceAtOrder()).isEqualTo(200);
+        assertThat(orderItemCaptor.getAllValues().get(1).getQuantity()).isEqualTo(4);
     }
     
     @Test
--- 1972-shopping-test-service/src/test/java/com/example/shopping/service/ProductMaintenanceServiceImplTest.java
+++ 1972-shopping-test-service/src/test/java/com/example/shopping/service/ProductMaintenanceServiceImplTest.java
@@ -4,13 +4,17 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.OptimisticLockingFailureException;
 
+import com.example.shopping.entity.Product;
 import com.example.shopping.input.ProductMaintenanceInput;
 import com.example.shopping.repository.ProductRepository;
 
@@ -24,15 +28,20 @@ class ProductMaintenanceServiceImplTest {
 
     @Test
     public void test_update() {
-        doReturn(true).when(productRepository).update(any());
+        ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
+        doReturn(true).when(productRepository).update(productCaptor.capture());
         
         ProductMaintenanceInput productMaintenanceInput = new ProductMaintenanceInput();
         productMaintenanceInput.setId("p01");
         productMaintenanceInput.setName("pname01");
         productMaintenanceInput.setPrice(100);
         productMaintenanceInput.setStock(10);
-        
         productMaintenanceService.update(productMaintenanceInput);
+        
+        assertThat(productCaptor.getValue().getId()).isEqualTo("p01");
+        assertThat(productCaptor.getValue().getName()).isEqualTo("pname01");
+        assertThat(productCaptor.getValue().getPrice()).isEqualTo(100);
+        assertThat(productCaptor.getValue().getStock()).isEqualTo(10);
     }
 
     @Test
@@ -43,5 +52,27 @@ class ProductMaintenanceServiceImplTest {
         assertThatThrownBy(() -> {
             productMaintenanceService.update(productMaintenanceInput);
         }).isInstanceOf(OptimisticLockingFailureException.class);
+    }
+    
+    @Test
+    public void test_findAll() {
+    	doReturn(List.of(new Product(), new Product(), new Product())).when(productRepository).selectAll();
+    	List<Product> products = productMaintenanceService.findAll();
+    	assertThat(products.size()).isEqualTo(3);
+    }
+    
+    public void test_findById() {
+		Product beforeProduct = new Product();
+		beforeProduct.setId("p02");
+		beforeProduct.setName("ノート(改)");
+		beforeProduct.setPrice(2000);
+		beforeProduct.setStock(200);
+		doReturn(beforeProduct).when(productRepository).selectById("p02");
+		
+		Product afterProduct = productMaintenanceService.findById("p02");
+		assertThat(afterProduct.getId()).isEqualTo("p02");
+		assertThat(afterProduct.getName()).isEqualTo("ノート(改)");
+		assertThat(afterProduct.getPrice()).isEqualTo(2000);
+		assertThat(afterProduct.getStock()).isEqualTo(200);
     }
 }
